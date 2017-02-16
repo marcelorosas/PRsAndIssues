d844e11c37508ad38bd19e016d92fd82a6b59209
Marcelo Rosas <marcelo.rosas@globant.com>
2017-02-16 18:00:01 -0300

    Adding apache commons cli to better handling parameters

--- src/main/java/com/metromile/Main.java
+++ src/main/java/com/metromile/Main.java
@@ -4,27 +4,65 @@ import com.jayway.jsonpath.JsonPath;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
+import org.apache.commons.cli.*;
+
 import java.util.List;
 
 public class Main {
 
+    private static String path = "";
+
     public static void main(String[] args) {
         try {
 
-            String path = "yourUrl";
+            path = "https://stash.com/rest/api/latest/projects/MET/repos/";
+            Options options = new Options();
 
-            if(args.length > 0) {
-                if (args[0] != null) {
-                    path += "&state=" + args[0];
-                }
+            Option stateOption = Option.builder("state")
+                    .required(false)
+                    .desc("state of the PR (ALL, MERGED, OPEN")
+                    .hasArg()
+                    .build();
+
+            Option startOption = Option.builder("start")
+                    .required(false)
+                    .desc("start for the batch to retrieve, 0 to n")
+                    .hasArg()
+                    .build();
+
+            Option userOption = Option.builder("user")
+                    .required(false)
+                    .desc("user name, i.e marcelo.rosas")
+                    .hasArg()
+                    .build();
+
+            Option repoOption = Option.builder("repo")
+                    .required(true)
+                    .desc("repository, i.e salesforce, sugar, mms")
+                    .hasArg()
+                    .build();
+
+            options.addOption(stateOption);
+            options.addOption(startOption);
+            options.addOption(userOption);
+            options.addOption(repoOption);
+
+            CommandLineParser parser = new DefaultParser();
+            CommandLine cmd = parser.parse(options, args);
+
+            path += cmd.getOptionValue("repo");
+            path += "/pull-requests?order=newest&limit=100";
+
+            if (cmd.hasOption("state")) {
+                path += "&state=" + cmd.getOptionValue("state");
             }
 
-            if (args.length > 1) {
-                path += "&start=" + args[1];
+            if (cmd.hasOption("start")) {
+                path += "&start=" + cmd.getOptionValue("start");
             }
 
-            if (args.length > 2) {
-                path += "&role.1=AUTHOR&username.1=" + args[2];
+            if (cmd.hasOption("user")) {
+                path += "&role.1=AUTHOR&username.1=" + cmd.getOptionValue("user");
             }
 
             Client client = Client.create();
@@ -33,7 +71,7 @@ public class Main {
                     .resource(path);
 
             ClientResponse response = webResource.accept("application/json")
-                    .header("Authorization","I won't tell you.")
+                    .header("Authorization", "")
                     .get(ClientResponse.class);
 
             if (response.getStatus() != 200) {
@@ -44,16 +82,25 @@ public class Main {
             String output = response.getEntity(String.class);
             List<String> fromRefs = JsonPath.read(output, "$.values[*].fromRef.id");
 
-            System.out.println("Number of PRs: " + fromRefs.size());
+            System.out.println("");
+            System.out.println("*************************************************");
+            System.out.println("****** WELCOME TO PRs AND ISSUES ****************");
+            System.out.println("Hello Lucia, this is the endpoint you're hitting:");
+            System.out.println(path);
+            System.out.println("Here's the total number of PRs: " + fromRefs.size());
+            System.out.println("*************************************************");
 
-            for (String id: fromRefs) {
+            System.out.println("");
+            System.out.println("And here they are:");
+
+            for (String id : fromRefs) {
                 System.out.println("fromRef: " + id);
             }
 
+        } catch (MissingOptionException moe) {
+            System.out.println("Dear friend, remember that the -repo parameter is required :)");
         } catch (Exception e) {
-
             e.printStackTrace();
-
         }
     }
 }
